7f752676dc87e4c28399c7a0328e887d8f200e87
Xing Ye <yxinghang@gmail.com>
2020-10-28 14:09:30 -0400

    bubble sort function finished

--- bubble_selection_Insertion_sort.py
+++ bubble_selection_Insertion_sort.py
@@ -14,12 +14,25 @@ def is_sorted(items):
         #return true if current item is less than next item
     return True
 def bubble_sort(items):
-    """Sort given items by swapping adjacent items that are out of order, and
-    repeating until all items are in sorted order.
+    """
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    #minus one because there is not an item after the comparison of last item
+    items_length = len(items) - 1
+    #use sorted function to break the loop when the list is being sorted
+    sorted = False
+    #as long as is_sorted is False, while loop continues to run until its true 
+    while not sorted:
+        sorted = True
+        for i in range(0,items_length):
+            if items[i] > items[i+1]:
+                sorted = False
+                items[i], items[i+1] = items[i+1], items[i]
+                #use i to go through the items, check is current items is greater than or less than next item
+                #if True, stop the loop, if false, swap the two items
+                #if all items are sorted, is sorted function will return, if condition would not activate
+                # so the is_sorted will stay True
+    return items
 
 
 def selection_sort(items):
@@ -41,5 +54,10 @@ def insertion_sort(items):
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
 
-print(is_sorted([1,3,5,10,20]))#expecting True
-print(is_sorted([3,5,10,20,1]))#expecting False
+
+if __name__ == "__main__":
+    
+    # print(is_sorted([1,3,5,10,20]))#expecting True
+    # print(is_sorted([3,5,10,20,1]))#expecting False
+
+    #print(bubble_sort([1,4,5,2])) #expecting 1,2,4,5

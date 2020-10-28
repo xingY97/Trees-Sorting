669d2709dc49cf036b204c868b4fb93983ef6812
Xing Ye <yxinghang@gmail.com>
2020-10-28 13:41:32 -0400

    is_sorted function added

--- bubble_selection_Insertion_sort.py
+++ bubble_selection_Insertion_sort.py
@@ -3,8 +3,16 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
-
-
+    for i in range(len(items) - 1):
+        current_item = items[i]
+        next_item = items[i+1]
+        
+        #checking for ascending order, so the current item need to be less than next item
+        #if next item is less than current item then it is not sorted. return false
+        if next_item <= current_item:
+            return False
+        #return true if current item is less than next item
+    return True
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
@@ -32,3 +40,6 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+
+print(is_sorted([1,3,5,10,20]))#expecting True
+print(is_sorted([3,5,10,20,1]))#expecting False

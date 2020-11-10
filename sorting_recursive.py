198be401af1953f814432265b5e2e42d9c4b0a34
Xing Ye <yxinghang@gmail.com>
2020-11-09 19:10:31 -0500

    merge function complete

--- sorting_recursive.py
+++ sorting_recursive.py
@@ -6,9 +6,33 @@ def merge(items1, items2):
     and return a new list containing all items in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until one list is empty
     # TODO: Find minimum item in both lists and append it to new list
+    #since we assumed two lists are already sorted, if items1 is empty return items2, vice versa
+    #and then create a empty new list to append items from items1 and 2
+    if len(items1) == 0:
+        return items2
+    if len(items2) == 0:
+        return items1
+    new_list = []
+
+    # through the lists from index 0
+    i,j = 0,0
+    #we have to Find minimum item in both lists and append it to new list
+    #use a while loop so it repeats until one list is empty
+    #if first item in list1 is smaller than first item in list 2, append first item in list 1 to the new list, else, append item from list 2 to the new list, and then check
+    #the next element in the list until the list is empty, so we do i += 1
+    while len(items1) > i and len(items2) > j:
+        if items1[i] < items2[j]:
+            new_list.append(items1[i])
+            i += 1
+        else:
+            new_list.append(items2[j])
+            j += 1
     # TODO: Append remaining items in non-empty list to new list
+    #extend method adds all iterable element to the end of new list
+    new_list.extend(items1[i:])
+    new_list.extend(items2[j:])
+    
 
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
@@ -45,3 +69,11 @@ def quick_sort(items, low=None, high=None):
     # TODO: Check if list or range is so small it's already sorted (base case)
     # TODO: Partition items in-place around a pivot and get index of pivot
     # TODO: Sort each sublist range by recursively calling quick sort
+
+
+
+if __name__ =="__main__":
+    list1 = [1,3,5,7,9]
+    list2 = [2,4,6,8,10]
+    
+    print(merge()
